56f08451a206953c88afb9ef67d90557b760690b
yuki noguchi <131225436+nyuhki@users.noreply.github.com>
2024-01-30 14:12:59 +0900

    Add files via upload

---
+++ lightSra2Count/lightSra2Count.sh
@@ -0,0 +1,21 @@
+###########################################################################
+#
+#Current Version is Deposited to `https://github.com/SuzukiLab-icems/Sra2Count/tree/main/lightSra2Count`.
+#Future update will be conducted in this Repository.
+#
+#sra2count/lightSra2Count.sh
+#
+#	 Copyright (c) 2024, Noguchi Yuki (Jun Suzuki lab)
+#	 This software is released under the MIT License, see LICENSE (https://opensource.org/license/mit/).
+#    @citation: Noguchi, Y., Onodera, Y., Maruoka, M., Miyamoto, T., Kosako, H., Suzuki., J. 2024. In vivo CRISPR screening directly targeting testicular cells. Cell Genomics.
+#    @author:  Noguchi Yuki
+#    @contact: nyuhki21@gmail.com,jsuzuki@icems.kyoto-u.ac.jp
+#
+##REFERENCE
+#1. FastQC:https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+#2.	SRA toolkit:https://hpc.nih.gov/apps/sratoolkit.html
+#3. TrimGalore:https://www.bioinformatics.babraham.ac.uk/projects/trim_galore/
+#4. Pertea, M., Kim, D., Pertea, G.M., Leek, J.T., and Salzberg, S.L. (2016). Transcript-level expression analysis of RNA-seq experiments with HISAT, StringTie and Ballgown. Nat Protoc 11, 1650–1667. 10.1038/nprot.2016.095.
+#5. Pertea, M., Pertea, G.M., Antonescu, C.M., Chang, T.C., Mendell, J.T., and Salzberg, S.L. (2015). StringTie enables improved reconstruction of a transcriptome from RNA-seq reads. Nat Biotechnol 33, 290–295. 10.1038/nbt.3122.
+#6. Li, H., Handsaker, B., Wysoker, A., Fennell, T., Ruan, J., Homer, N., Marth, G., Abecasis, G., Durbin, R., and 1000 Genome Project Data Processing Subgroup (2009). The Sequence Alignment/Map format and SAMtools. Bioinformatics 25, 2078–2079. 10.1093/bioinformatics/btp352.
+###########################################################################
